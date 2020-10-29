b57da897348f711991d3ee7d91c4342c5ffbd3a9
xenarki <xenarki@gmail.com>
2020-10-29 17:13:48 +0700

    Добавлен новый класс

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,25 @@
 public class Stack {
+    private int tos;
+    private char stck[];
+
+    Stack(int size) {
+        stck = new char[size];
+        tos = 0;
+    }
+
+    Stack(Stack ob) {
+        tos = ob.tos;
+        stck = new char[ob.stck.length];
+        for (int i; i < tos; i++) {
+            stck[i] = ob.stck[i];
+        }
+    }
+
+    Stack(char a[]) {
+        tos = a.length;
+        stck = new char[a.length];
+        for (int i = 0; i < tos; i++) {
+            stck[i] = a[i];
+        }
+    }
 }
